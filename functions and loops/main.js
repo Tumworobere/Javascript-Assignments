6dc471fad34544f3f182412e9fe22fd14500ebb7
Tumworobere <annaht.annah@gmail.com>
2023-12-05 12:43:31 +0400

    Created a function that checks even numbers

---
+++ functions and loops/main.js
@@ -0,0 +1,13 @@
+//Create a function that checks if the input/parameter is an even number.
+function checkEvenNumber(number) {
+    if (number % 2 === 0) {
+        console.log(`${number} is an even number.`);
+    } else {
+        console.log(`${number} is not an even number.`);
+    }
+}
+
+checkEvenNumber(11);
+
+
+//create a function that prints to the console n1 to n2 where n1 is the first parameter and n2 is the second parameter
