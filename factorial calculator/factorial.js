7428c8c3a92e57b69d81370dca6add389f808208
Tumworobere <annaht.annah@gmail.com>
2023-12-06 12:48:30 +0400

    Calculated the factorial of a given number

---
+++ factorial calculator/factorial.js
@@ -0,0 +1,21 @@
+function calculateFactorial(number) {
+    // Check for negative numbers
+    if (number < 0) {
+        return "Factorial is not defined for negative numbers.";
+    } else if (number === 0 || number === 1) {
+        // Factorial of 0 and 1 is 1
+        return 1;
+    } else {
+        // Calculate factorial for numbers greater than 1
+        let result = 1;
+        for (let i = 2; i <= number; i++) {
+            result *= i;
+        }
+        return result;
+    }
+}
+
+// Example usage
+const inputNumber = 10;
+const factorialResult = calculateFactorial(inputNumber);
+console.log(`The factorial of ${inputNumber} is: ${factorialResult}`);
